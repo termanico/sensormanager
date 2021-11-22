b6ae2acc5f0339a8541d6f2f784d724e7b13a508
Klingbeil-Thorsten <klingbeil.thorsten@swm.de>
2021-11-22 10:04:53 +0100

    u

--- webapp/control/Thermometer.js
+++ webapp/control/Thermometer.js
@@ -13,13 +13,28 @@ sap.ui.define([
     renderer: {
       apiVersion: 2,
       render: function (oRM, oControl) {
-        if (oControl.getValue()) {
-          oRM.openStart("div", oControl);
-          oRM.class("thermometer-control");
-          oRM.openEnd();
-          oRM.text(oControl.getValue());
-          oRM.close("div");
-        }
+        oRM.openStart("figure", oControl);
+        oRM.class("thermometer");
+        oRM.style("border", "2px solid " + oControl.getColor());
+        oRM.openEnd();
+
+        oRM.openStart("figcaption");
+        oRM.class("thermometer-value");
+        oRM.style("background-color", oControl.getColor());
+        oRM.style("box-shadow", "0 0 0 2px " + oControl.getColor());
+        oRM.openEnd();
+        oRM.text(oControl.getValue().toFixed(1)); // the temperature value
+        oRM.close("figcaption");
+
+        oRM.openStart("div");
+        oRM.class("thermometer-level");
+        var temperatureHeight = Math.min(oControl.getValue() * 7, 50) + 5; // values should range from 5 to 55
+        oRM.style("height", temperatureHeight + "px");
+        oRM.style("background-color", oControl.getColor());
+        oRM.openEnd();
+        oRM.close("div");
+
+        oRM.close("figure");
       }
     }
   });
